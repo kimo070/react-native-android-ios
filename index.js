4139d285f870a16135962e3c96cca90f1dcbfe43
Sujan Shrestha <nepkoder@gmail.com>
2019-04-18 17:31:49 +0545

    react-native project init

--- index.js
+++ index.js
@@ -6,10 +6,4 @@ import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
-import applyDecoratedDescriptor from '@babel/runtime/helpers/es6/applyDecoratedDescriptor'
-import initializerDefineProperty from '@babel/runtime/helpers/es6/initializerDefineProperty'
-
-Object.assign(babelHelpers, {applyDecoratedDescriptor, initializerDefineProperty})
-
-
 AppRegistry.registerComponent(appName, () => App);
